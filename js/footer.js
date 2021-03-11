fc66d616dce1595911e32654a1383980818de674
南紗依子 <>
2021-03-11 13:11:38 +0900

    code　アップロードしました

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+let j$ = jQuery,
+$nav = j$(".menu"),
+$slideLine = j$("#slide-line"),
+$currentItem = j$(".current-item");
+
+j$(function(){  
+// Menu has active item
+if ($currentItem[0]) {
+$slideLine.css({
+  "width": $currentItem.width(),
+  "left": $currentItem.position().left
+});
+}
+
+// Underline transition
+j$($nav).find("li").hover(
+// Hover on
+function(){
+  $slideLine.css({
+    "width": j$(this).width(),
+    "left": j$(this).position().left
+  });
+},
+// Hover out
+function(){
+  if ($currentItem[0]) {
+    // Go back to current
+    $slideLine.css({
+      "width": $currentItem.width(),
+      "left": $currentItem.position().left
+    });
+  } else {
+    // Disapear
+    $slideLine.width(0);
+  }
+}
+);
+});
+
